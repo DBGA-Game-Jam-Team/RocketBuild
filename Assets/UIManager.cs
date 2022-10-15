46efa304b7652f53c92507ce4423c05830bcde98
nico-bertoli <nicobertoli.1999@gmail.com>
2022-10-15 15:36:16 +0200

    distance text added

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : Singleton<UIManager>
 {
@@ -8,6 +9,7 @@ public class UIManager : Singleton<UIManager>
     [SerializeField] GameObject LaunchButton;
     [SerializeField] CountDownText countDownText;
     [SerializeField] GameObject GameInfoPanel;
+    [SerializeField] TextMeshProUGUI distanceText;
 
     public void ShowGameInfoPanel(bool _show)
     {
@@ -21,5 +23,9 @@ public class UIManager : Singleton<UIManager>
 
     public void StartCountDownText(int _time) {
         countDownText.StartCountDown(_time);
+    }
+
+    public void ShowDistance(int distance) {
+        distanceText.text=distance.ToString();
     }
 }
