bcc44bf5a896f1ac06c756921a9c0bdc91284d9a
nico-bertoli <nicobertoli.1999@gmail.com>
2022-10-16 00:04:45 +0200

    basic stats in ui

--- Assets/Script/UI/ComponentUI.cs
+++ Assets/Script/UI/ComponentUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ComponentUI : MonoBehaviour
 {
@@ -9,15 +10,44 @@ public class ComponentUI : MonoBehaviour
 
     [SerializeField]
     private Image componentImage;
+    [SerializeField]
+    private TextMeshProUGUI infoText;
+
 
     public void SetComponentUI(MyComponent comp)
     {
         component = comp;
         componentImage.sprite = comp.ComponenetSprite;
+
+        infoText.text = "";
+        //THRUSTER
+        if (component is Thruster) {
+            Thruster thruster = (Thruster)component;
+            infoText.text = "S: " + thruster.YSpeed;
+      }
+        //BODY
+        else if (comp is Body) {
+            Body body = (Body)comp;
+            infoText.text = "L: " + body.Life + "F: "+body.Fuel;
+
+        }
+        // TIP
+        else if (comp is Tip){
+            string weapon = null;
+            Tip tip = (Tip)component;
+            if (tip.Weapon) {
+                if (tip.Weapon.GetComponent<Weapon>()) weapon = "1";
+                else if (tip.Weapon.GetComponent<TripleWeapon>()) weapon = "2";
+            }
+            else weapon = "0";
+
+            infoText.text = "L: " + tip.Life.ToString() + "D: " + weapon;
+        }
     }
 
     public void EquipComponent()
     {
+
         if(component is Thruster)
         {
             Thruster thruster = (Thruster)component;
