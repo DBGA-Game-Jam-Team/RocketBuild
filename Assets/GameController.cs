683c1fa039817ce21de65663dd4b090c1c591f34
nico-bertoli <nicobertoli.1999@gmail.com>
2022-10-15 15:07:19 +0200

    margins added

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameController : Singleton<GameController>
 {
+    public float XLeftMargin { get { return leftMargin.position.x; } }
+    public float XRightMargin { get { return rightMargin.position.x; } }
+    [SerializeField] Transform rightMargin;
+    [SerializeField] Transform leftMargin;  
     public void StartLaunch() {
         if (Rocket.Instance.ReadyToLaunch()) {
             Rocket.Instance.Launch();
