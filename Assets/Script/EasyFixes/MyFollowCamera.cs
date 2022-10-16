378758b621c6af16d7aa5f434ac2585af992ae5d
nico-bertoli <nicobertoli.1999@gmail.com>
2022-10-16 11:22:37 +0200

    Revert "camera offset added"
    This reverts commit a57e71443faaf108478027efbec020c4afd834bf.

--- Assets/Script/EasyFixes/MyFollowCamera.cs
+++ Assets/Script/EasyFixes/MyFollowCamera.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class MyFollowCamera : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField] float offset;
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(player.position.x, player.position.y+offset, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, player.position.y+offset, transform.position.z);
+        transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
     }
 }
